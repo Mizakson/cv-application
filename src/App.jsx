54caf47db7724ef6e74192b651c4f754f30d937d
Mizakson <maxwellizakson@gmail.com>
2024-08-26 14:45:25 -0400

    Create Header child elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import './styles/App.css'
+import Header from "./components/Header.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  console.log("OK...")
   return (
-    <div>Hello There...</div>
+   <Header />
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <h1 className="main-title">CV Application</h1>
+            <div className="about">
+                <p className="project-number"><a href="www.theodinproject.com">Odin Project</a> React Course Project #1</p>
+                <p className="author">An app made by <a href="www.github.com/mizakson">Mizakson</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
