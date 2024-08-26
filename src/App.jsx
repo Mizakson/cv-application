75f48fe8e633681cfebcbd706e0f7bc5f1264312
Mizakson <maxwellizakson@gmail.com>
2024-08-26 14:56:38 -0400

    Style header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // import { useState } from 'react'
 import './styles/App.css'
+import './styles/index.css'
 import Header from "./components/Header.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  console.log("OK...")
   return (
    <Header />
   )
