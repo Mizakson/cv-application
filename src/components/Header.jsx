6e83fd3ae192c6a7065bad8976ef64808e847669
Mizakson <maxwellizakson@gmail.com>
2024-08-27 11:45:02 -0400

    Remove header anchor tags, edit header width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ function Header() {
         <header>
             <h1 className="main-title">CV Application</h1>
             <div className="about">
-                <p className="project-number"><a href="www.theodinproject.com" title="www.theodinproject.com">Odin Project</a> React Course Project #1</p>
-                <p className="author">An app made by <a href="www.github.com/mizakson" title="www.github.com/mizakson">Mizakson</a></p>
+                <p className="project-number">Odin Project React Course Project #1</p>
+                <p className="author">An app made by Mizakson</p>
             </div>
         </header>
     )
