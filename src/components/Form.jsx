f236d5d59f1c47bfa28e937ddb711644c48ef445
Mizakson <maxwellizakson@gmail.com>
2024-08-29 09:58:29 -0400

    Create general info form section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,24 @@
 function Form() {
     return (
         <form>
-            <section>general info</section>
-            <section>education</section>
-            <section>work experience</section>
-            <section>personal projects</section>
-            <section>submit</section>
+            <section>
+                <p className="section-title">General Info</p>
+                <fieldset>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" id="name"/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" placeholder="example@me.com"/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="phone-number">Phone: </label>
+                    <input type="tel" name="phone-number" id="phone-number" placeholder="111-222-3333"/>
+                </fieldset>
+            </section>
+            <section></section>
+            <section></section>
+            <button type="submit">Submit</button>
         </form>
     )
 }
