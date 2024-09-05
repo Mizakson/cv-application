f0ad9eb6cab8dde2bbd615c742e774fd4dbb8701
Mizakson <maxwellizakson@gmail.com>
2024-09-05 12:07:37 -0400

    Add form input field labels, placeholders

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,45 +8,46 @@ export default function Form() {
             <legend>{titles[0]}</legend>
             <div className="input-field-wrapper">
                 <label htmlFor="name">Name: </label>
-                <input type="text" />
+                <input type="text" id="name"/>
             </div>
             <div className="input-field-wrapper"></div>
                 <label htmlFor="email">Email: </label>
-                <input type="email" placeholder="example@me.com"/>
+                <input type="email" id="email" placeholder="example@me.com"/>
             <div className="input-field-wrapper">
                 <label htmlFor="phone">Phone: </label>
-                <input type="tel" placeholder="111-222-3333"/>
+                <input type="tel" id='phone' placeholder="111-222-3333"/>
             </div>
         </fieldset>
         <fieldset>
             <legend>{titles[1]}</legend>
             <div className="input-field-wrapper">
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="school">School: </label>
+                <input type="text" id="school"/>
             </div>
             <div className="input-field-wrapper">
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="degree">Degree: </label>
+                <input type="text" id="degree"/>
             </div>
             <div className="input-field-wrapper">
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="study-date">Date: </label>
+                <input type="text" id="study-date" placeholder="AUG 2018 - MAY 2022"/>
             </div>
         </fieldset>
         <fieldset>
             <legend>{titles[2]}</legend>
             <div className="input-field-wrapper">
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="company">Company: </label>
+                <input type="text" id="company"/>
             </div>
             <div className="input-field-wrapper">
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="work-date">Date: </label>
+                <input type="text" id="work-date" placeholder="SEP 2023 - PRESENT"/>
             </div>
             <div className="input-field-wrapper">
-                <label htmlFor=""></label>
-                <input type="text" />
+                <label htmlFor="responsibilities">Responsibilities: </label>
+                <textarea type="text" placeholder="Developed feature XYZ..."/>
             </div>
+
         </fieldset>
         </div>
     )
