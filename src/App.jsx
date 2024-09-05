ebc907f6e566c3a5a5088a7cf7b87c758e83e2e9
Mizakson <maxwellizakson@gmail.com>
2024-09-05 11:02:34 -0400

    Wipe files, fresh project start

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './styles/App.css'
 import './styles/index.css'
-import Header from "./components/Header.jsx";
-import { Form } from './components/Form.jsx';
-import { Cv } from './components/Cv.jsx';
 
 function App() {
-
-  const [activeIndex, setActiveIndex] = useState(0);
+  
+  // const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <>
-      <div className='left'>
-        <Header />
-        <Form isActive={setActiveIndex === 0}
-        onClick={console.log(`active: ${activeIndex}`)}
-        />
-      </div>
-      <div className="right">
-        <Cv isActive={setActiveIndex === 1}
-        onClick={console.log(`active: ${activeIndex}`)}
-        />
-      </div>
+      <h1>Hello</h1>
     </>
   )
 }
--- src/components/Cv.jsx
+++
@@ -1,9 +0,0 @@
-export function Cv({ isActive, onClick }) {
-    return (
-        <div className="cv-display">
-            <div id="paper"></div>
-            <button id="edit">Edit</button>
-        </div>
-    )
-
-}
