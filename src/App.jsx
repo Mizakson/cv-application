83bb5fa16ca60a1965bbe104e0eaf1df737e4326
Mizakson <maxwellizakson@gmail.com>
2024-09-11 10:28:02 -0400

    Clean .jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,18 @@
 import './styles/App.css'
 import './styles/index.css'
 import Description from './components/Description'
-import Form from './components/Form'
-import Cv from './components/Cv'
+// import Form from './components/Form'
+// import Cv from './components/Cv'
 
 function App() {
 
   // const [isActive, setIsActive] = useState()
-
-  function handleSubmit(e) {
-      e.preventDefault()
-      document.querySelector("form").classList.toggle("covered")
-      document.querySelector(".display").classList.toggle("covered")
-  }
-
-  function handleClick(e) { 
-      e.preventDefault()
-      document.querySelector("form").classList.toggle("covered")
-      document.querySelector(".display").classList.toggle("covered")
-  }
 
 
 
   return (
     <>
       <Description />
-      <div className="wrapper">
-        <Form onSubmit={handleSubmit}/>
-        <Cv onClick={handleClick}/>
-      </div>
     </>
   )
 }
--- src/components/Cv.jsx
+++
@@ -1,9 +0,0 @@
-export default function Cv({ onClick }) {
-    
-    return (
-        <div className="display covered">
-            <div className="paper"></div>
-            <button className="edit" onClick={onClick}>Edit</button>  
-        </div>
-    )
-}
